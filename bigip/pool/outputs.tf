60c1125c99b07af9c8f0a5ffd527ac135a316229
張旭 <zx1986@gmail.com>
2020-04-22 21:53:07 +0800

    fix: 修改 output 的變數名稱，加上前綴

--- bigip/pool/outputs.tf
+++ bigip/pool/outputs.tf
@@ -1,9 +1,9 @@
-output "nodes" {
+output "bigip_nodes" {
   description = "建立出來的 Nodes"
   value       = values(bigip_ltm_node.nodes)[*]
 }
 
-output "pool" {
+output "bigip_pool" {
   description = "建立出來的 Pool"
   value       = bigip_ltm_pool.pool
 }
