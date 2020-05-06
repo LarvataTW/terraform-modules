edf808b5a64f0d7e5ee208b5576bddcedb5445d4
張旭 <zx1986@gmail.com>
2020-05-06 22:00:23 +0800

    fix: 補齊要傳入 JSON template 的參數設定

--- bigip/as3/main.tf
+++ bigip/as3/main.tf
@@ -5,9 +5,20 @@ resource "bigip_as3" "as3" {
   as3_json = templatefile(
     "${path.module}/templates/as3.tmpl",
     {
-      tenant_name       = jsonencode(var.tenant_name)
-      virtual_addresses = jsonencode([for ip in var.virtual_server_addresses : "${ip}"])
-      server_addresses  = jsonencode([for ip in var.node_addresses : "${ip}"])
+      as3_action               = jsonencode(var.as3_action)
+      as3_id                   = jsonencode(var.as3_id)
+      as3_lable                = jsonencode(var.as3_lable)
+      as3_remark               = jsonencode(var.as3_remark)
+      as3_application_name     = jsonencode(var.as3_application_name)
+      as3_application_template = jsonencode(var.as3_application_template)
+      as3_service_class        = jsonencode(var.as3_service_class)
+      tenant_name              = jsonencode(var.tenant_name)
+      pool_name                = jsonencode(var.pool_name)
+      pool_mode                = jsonencode(var.pool_mode)
+      pool_description         = jsonencode(var.pool_description)
+      node_port                = jsonencode(var.node_port)
+      server_addresses         = jsonencode([for ip in var.node_addresses : "${ip}"])
+      virtual_addresses        = jsonencode([for ip in var.virtual_server_addresses : "${ip}"])
     }
   )
 }
