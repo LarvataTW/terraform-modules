06d8554230ac4ea53e7bf0605102ef4ed246be93
Albert Tseng <albert@AlbertdeMacBook-Pro.local>
2020-08-07 08:10:26 +0800

    update

--- bigip/pool/outputs.tf
+++ bigip/pool/outputs.tf
@@ -1,7 +1,7 @@
-output "bigip_nodes" {
-  description = "建立出來的 Nodes"
-  value       = values(bigip_ltm_node.nodes)[*]
-}
+#output "bigip_nodes" {
+#  description = "建立出來的 Nodes"
+#  value       = values(bigip_ltm_node.nodes)[*]
+#}
 
 #output "bigip_pool" {
 #  description = "建立出來的 Pool"
