2416e0498df8398a89368ae9c94b022b55880169
Albert Tseng <albert@AlbertdeMacBook-Pro.local>
2020-09-24 19:00:15 +0800

    更新modules

--- aws/programmatic_access_users/outputs.tf
+++ aws/programmatic_access_users/outputs.tf
@@ -6,10 +6,6 @@ output "created_users" {
   value = values(aws_iam_user._users)[*]
 }
 
-output "created_user_passwords" {
-  value = values(aws_iam_user_login_profile._user_passwords)[*]
-}
-
 output "created_policies" {
   value = values(aws_iam_policy._policies)[*]
 }
